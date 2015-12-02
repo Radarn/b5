b454970152d77cb9986aa2ebca3426ac2a16fa0a
Radarn <admin@Arthurs-dator.local>
2015-12-02 14:59:23 +0100

    Added fadeIn and fadeOut with jQuery

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ for (var i = 0; i < h3.length; i+=1) {
 	h3[i].style.display="none";
 }
 
+
 button[0].addEventListener("click", showText5);
 button[1].addEventListener("click", showText4);
 button[2].addEventListener("click", showText3);
@@ -12,43 +13,48 @@ button[3].addEventListener("click", showText2);
 button[4].addEventListener("click", showText1);
 
 
-
 function showText5() {
 	if (h3[0].style.display === "none") {
-		h3[0].style.display = "";
+		$(".1").fadeIn("slow");
 	} else {
-		h3[0].style.display = "none";
+		$(".1").fadeOut("slow");
 	}
 }
 
 function showText4() {
 	if (h3[1].style.display === "none") {
-		h3[1].style.display = "";
+		$(".2").fadeIn("slow");
 	} else {
-		h3[1].style.display = "none";
+		$(".2").fadeOut("slow");
 	}
 }
 
 function showText3() {
 	if (h3[2].style.display === "none") {
-		h3[2].style.display = "";
+		$(".3").fadeIn("slow");
 	} else {
-		h3[2].style.display = "none";
+		$(".3").fadeOut("slow");
 	}
 }
 
 function showText2() {
 	if (h3[3].style.display === "none") {
-		h3[3].style.display = "";
+		$(".4").fadeIn("slow");
 	} else {
-		h3[3].style.display = "none";
+		$(".4").fadeOut("slow");
 	}
 }
 
 function showText1() {
 	if (h3[4].style.display === "none") {
-		h3[4].style.display = "";
+		$(".5").fadeIn("slow");
 	} else {
-		h3[4].style.display = "none";
+		$(".5").fadeOut("slow");
 	}
 }
+
+/*$("span").click(changeBackground);
+
+function changeBackground() {
+	$("body").style.backgroundColor = ;
+}*/
